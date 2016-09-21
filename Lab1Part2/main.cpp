7224b4396327e147faf74a70e8e6d92eb25d388b
B00067973 <B00067973@student.itb.ie>
2016-09-21 12:19:54 +0100

    First Part

---
+++ Lab1Part2/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+// Base class
+class Shape
+{
+public:
+ // pure virtual function providing interface framework.
+ virtual int getArea() = 0;
+ void setWidth(int w)
+ {
+ width = w;
+ }
+ void setHeight(int h)
+ {
+ height = h;
+ }
+protected:
+ int width;
+ int height;
+};
+class Triangle: public Shape
+{
+public:
+ int getArea()
+ {
+ return (width * height)/2;
+ }
+};
+class Rectangle: public Shape
+{
+public:
+ int getArea()
+ {
+ return (width * height);
+ }
+};
+int main(void)
+{
+ Triangle Tri;
+ Rectangle Rect;
+ Tri.setWidth(5);
+ Tri.setHeight(7);
+ Rect.setHeight(4);
+ Rect.setWidth(6);
+ // Print the area of the object.
+ cout << "Total Triangle area: " << Tri.getArea() << endl;
+ cout << "Total Rectangle Area: "<< Rect.getArea() <<endl;
+ return 0;
+}
